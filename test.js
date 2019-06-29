5b5c7b858c1daf55ec81b72a4d09f33129d544f6
ChaA'llisa Taylor <chaallisat@yahoo.com>
2019-06-29 12:18:20 -0700

    added variable for user interaction

--- test.js
+++ test.js
@@ -1,6 +1,12 @@
 
+const userSearch 
+const diet
+const health
+const excludedIngredients
+const minCalorie
+const maxCalorie
 
-const queryURL = "https://api.edamam.com/search?q=chicken&app_id=ff5eea8f&app_key=60ed34c1d6d2b78f3933b4438a735801&ingr=4";
+const queryURL = "https://api.edamam.com/search?q=" + userSearch + "&app_id=ff5eea8f&app_key=60ed34c1d6d2b78f3933b4438a735801&from=1&to=2&calories=" + minCalorie + "-" + maxCalorie + "&health=" + health + "&diet=" + diet + "&excluded=" + excludedIngredients;
 //"https://api.giphy.com/v1/gifs/search?q=" +
 //animal + "&api_key=dc6zaTOxFJmzC&limit=10";
 
@@ -19,5 +25,10 @@ method: "GET"
         const name = response.hits[i].recipe.label;
         console.log(name);
     }
+
+})
+
+$(someID).on("click", function(event){
+    event.preventDefault();
 
 })
