252b14603a09a55e76071a0b1ee60d5ea92ef13d
ChaA'llisa Taylor <chaallisat@yahoo.com>
2019-07-06 13:24:43 -0700

    appp.js and index.html are the main docs. You can ignore the test docs.

--- test.js
+++ test.js
@@ -1,12 +1,105 @@
 
-function test() {
+
 
     //Seperate by seasons
 
-const userSearch = ["summer", "fall", "winter", "spring"];
+//const userSearch = ""
+//["summer", "fall", "winter", "spring"];
+/**
+ @returns {string}
+ * 
+ */
 
+function buildURL() {
 
-const queryURL = "https://api.edamam.com/search?q=" + userSearch + "&app_id=ff5eea8f&app_key=60ed34c1d6d2b78f3933b4438a735801";
+const queryURL = "https://api.edamam.com/search?";
+
+const queryParams = { "app_id": "ff5eea8f", "app_key": "60ed34c1d6d2b78f3933b4438a735801" };
+queryParams.q = $("#recipeSearch")
+.val()
+.trim();
+
+console.log("---------------\nURL: " + queryURL + "\n---------------")
+console.log(queryURL + $.param(queryParams));
+return queryURL + $.param(queryParams);
+
+}
+
+function updatePage(edamamRecipe) {
+    const numRecipe = edamamRecipe.hits.length;
+
+    console.log(edamamRecipe);
+    console.log("------------------------------------");
+  
+    // Loop through and build elements for the defined number of articles
+    for (let i = 0; i < numRecipe; i++) {
+      // Get specific article info for current index
+      const recipeV = edamamRecipe.hits[i].recipe;
+  
+      // Increase the articleCount (track article # - starting at 1)
+      //const articleCount = i + 1;
+  
+      // Create the  list group to contain the articles and add the article content for each
+      const $articleList = $("<ul>");
+      $articleList.addClass("list-group");
+  
+      // Add the newly created element to the DOM
+      $("#recipeDiv").append($articleList);
+  
+      // If the article has a headline, log and append to $articleList
+      /*const headline = recipeV.label;
+      const $articleListItem = $("<li class='list-group-item articleHeadline'>");
+  
+      if (headline && headline.main) {
+        console.log(headline.main);
+        $articleListItem.append(
+          "<span class='label label-primary'>" +
+            articleCount +
+            "</span>" +
+            "<strong> " +
+            headline.main +
+            "</strong>"
+        );
+      }*/
+  
+  
+      // Log section, and append to document if exists
+      const section = recipeV.label;
+      console.log(recipeV.label);
+
+      const $articleListItem = $("<div>").addClass("art");
+
+      if (section) {
+        $articleListItem.append("<h5>Section: " + section + "</h5>");
+      }
+  
+      const dataImg = recipeV.image;
+        //Create new image div and gave it an attribute
+        const imgDiv = $("<img class='image'>")
+        imgDiv.attr("src", dataImg);
+  
+      // Append and log url
+      $articleListItem.append("<a href='" + recipeV.uri + "'>" + recipeV.url + "</a>");
+      $articleListItem.append(imgDiv);
+      console.log(recipeV.url);
+  
+      // Append the article
+      $articleList.append($articleListItem);
+    }
+} 
+
+function clear() {
+    $("#article-section").empty();
+  }
+  
+  // .on("click") function associated with the Search Button
+  $("#run-search").on("click", function(event) {
+    event.preventDefault();
+  
+    clear();
+  
+    const queryURL = buildURL();
+  
 
 
 // Performing an AJAX request with the queryURL
@@ -15,9 +108,14 @@ url: queryURL,
 method: "GET"
 })
 // After data comes back from the request
-.then(function(response) {
+.then(updatePage);
+  });
+/*
+(function(response) {
 
     console.log(response);
+
+    userSearch = $("#recipeSearch").val().trim();
 
     for (i = 0; i < response.hits.length; i++) {
         //Variables for the recipe name, image, health labels, and diet labels
@@ -38,12 +136,12 @@ method: "GET"
         console.log(name);
         const newDiv = $("<h4>");
         //Append new divs to the recipe div in our html
-        $(".recipe").append(imgDiv);
-        $(".recipe").append(newDiv);
-        $(".recipe").append(healthDiet);
+        $("#recipeDiv").append(imgDiv);
+        $("#recipeDiv").append(newDiv);
+        $("#recipeDiv").append(healthDiet);
         
     }
 
 })
+*/
 
-}
